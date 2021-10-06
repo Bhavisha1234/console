027e3595eea337e424867cf1dcdb993e4b96f3f4
Bhavisha1234 <88883356+Bhavisha1234@users.noreply.github.com>
2021-10-06 18:28:53 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console=[];
+function add(){
+    var console=[];
+        var first_name="Bhavisha";
+        var last_name="Vazirani";
+        var a="10";
+        var b="5";
+        var add=a + b;
+        console.log(add);
+    }
